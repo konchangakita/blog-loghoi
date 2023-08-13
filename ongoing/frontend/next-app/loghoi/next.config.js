79d082b3fe065d015030ba2a3f71bcdfde2395e5
kon-chan <koncangakita@hotmail.com>
2023-08-14 02:49:07 +0900

    install nextjs

--- ongoing/frontend/next-app/loghoi/next.config.js
+++ ongoing/frontend/next-app/loghoi/next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  webpack: (config, context) => {
-    config.watchOptions = {
-      poll: 1200,
-      aggregateTimeout: 500,
-      ignored: ['**/node_modules', '**/.next'],
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
