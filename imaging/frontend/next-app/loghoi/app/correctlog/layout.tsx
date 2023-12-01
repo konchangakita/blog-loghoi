dcef034b9cd8f8d9bed18e8b1bb2e5a017d501a1
kon-chan <koncangakita@hotmail.com>
2023-12-02 02:03:24 +0900

    delete imaging

--- imaging/frontend/next-app/loghoi/app/correctlog/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'correct log - Log hoihoi',
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
-}
